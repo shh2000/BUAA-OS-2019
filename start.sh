280f45b0fd40f180a816fdbe0d6718b244f7ac13
shh2000 <13820618441@163.com>
2019-05-25 10:30:31 +0800

    finish boot,driver,rootfile

---
+++ start.sh
@@ -0,0 +1,5 @@
+#########################################################################
+# File Name: filestart.sh
+# Author: test
+/OSLAB/gxemul -E testmips -C R3000 -M 64 -d gxemul/fs.img gxemul/vmlinux
+
